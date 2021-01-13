69a983ec728b4054e5f3e4dc704a4f4b5fa3bf6c
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-13 13:00:05 -0500

    latest changes

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # your code goes here
+
+
+
+
+class Person
+    
+    attr_reader :name
+    attr_accessor :bank_account
+    
+
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+    end
+
+    def happiness=(happiness_index)
+        @happiness = happiness_index
+    end 
+
+
+end 
