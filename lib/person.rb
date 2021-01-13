b972c3b2773171f385b3c6404cd1c197a5545976
Joshua Morales <joshuapmorales@gmail.com>
2021-01-13 16:50:53 -0600

    commit tree 8125a9278815b08b7b11b542483684a2eb74aac7

--- lib/person.rb
+++ lib/person.rb
@@ -1,24 +1,23 @@
 # your code goes here
 
-
-
-
 class Person
     
     attr_reader :name
-    attr_accessor :bank_account
+    attr_accessor :bank_account, :happiness
     
-
-
-    def initialize(name)
+    def initialize(name, bank_account = 25, happiness = 8)
         @name = name
-        @bank_account = 25
-        @happiness = 8
+        @bank_account = bank_account
+        @happiness = happiness
     end
 
     def happiness=(happiness_index)
         @happiness = happiness_index
+        if @happiness < 0
+            return @happiness = 0
+        elsif @happiness > 10
+            return @happiness = 10
+        end
     end 
-
 
 end 
